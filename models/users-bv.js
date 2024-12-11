46e6e69772219adc2e78e9d1580a48c8c613ea32
migacur <78935454+migacur@users.noreply.github.com>
2024-12-11 12:18:47 -0400

    cambio again avatar...

--- models/users-bv.js
+++ models/users-bv.js
@@ -11,13 +11,7 @@ const userBVSchema = new Schema({
     },
     avatar: { 
         type: String,
-        default: "",
-        validate: {
-            validator: function(v) {
-                return v === "" || /^https:\/\//.test(v);
-            },
-            message: props => `${props.value} no es una URL segura (debe ser https://)!`
-        }
+        default: ""
     },
     email: {
         type: String, 
@@ -38,28 +32,22 @@ const userBVSchema = new Schema({
         type: Boolean,
         default: false
     },
-    favoritos: [{
+    favoritos:[{
        type: Schema.Types.ObjectId,
        ref: 'Books'
     }],
-    userCode: {
+     userCode: {
         type: String,
         default: null
-    }
+     }
 
-}, { versionKey: false });
+}, {versionKey: false});
 
-userBVSchema.pre('save', function(next) { 
-    if (this.avatar && this.avatar.startsWith('http://')) {
-        this.avatar = this.avatar.replace(/^http:\/\//, 'https://');
-    }
-    next();
-});
-
-// Excluir ciertas propiedades al convertir a JSON
 userBVSchema.methods.toJSON = function() {
-    const { password, state, ...user } = this.toObject();
+    const { password,state, ...user  } = this.toObject();
     return user;
-};
+}
 
-module.exports = model('Users', userBVSchema);
+
+
+module.exports = model( 'Users', userBVSchema)
